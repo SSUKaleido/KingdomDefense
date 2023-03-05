4e01d991f811154183ee6acb4651b8c4ba23b9c4
Jongeun <bubble2948@naver.com>
2023-03-05 23:00:54 +0900

    Track Enemy Adjoin

--- KingdomDefense/Assets/Scripts/Unit/Unit.cs
+++ KingdomDefense/Assets/Scripts/Unit/Unit.cs
@@ -1,31 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Unit : MonoBehaviour
 {
-    /*  유닛의 속성과 진영을 결정짓는 열거형  */
-    public enum ElementType {
-        KD_Normal, KD_Fire, KD_Ice, MO_Normal, MO_Fire, MO_Ice
+    /*  유닛의 속성과 진영을 결정짓는 열거형, 비트마스크 사용  */
+    [Flags] public enum ElementType {
+        KD_Normal = 1 << 0,
+        KD_Fire = 1 << 1,
+        KD_Ice = 1 << 2,
+        MO_Normal = 1 << 3,
+        MO_Fire = 1 << 4,
+        MO_Ice = 1 << 5
     }
+    /*  유닛의 직업을 결정짓는 열거형  */
+    [Flags] public enum JobType {
+        Building = 1 << 0,
+        Warrior = 1 << 1,
+        Achor = 1 << 2
+    }
+
     public ElementType elementType;
+    public JobType jobType;
+    public int hp;
 
     /* 메인 카메라와 부속 스크립트 컴포넌트  */
     private Camera mainCamera;
     private UnitMove unitMove;
     private UnitDetect unitDetect;
+    private UnitBattle unitBattle;
 
     private GameObject closestEnemy = null;
-    [SerializeField] private const float delay = 0.25f;
+    public bool isAdjoinWithEnemy = false;
+    private const float delay = 0.25f;
 
-    public void Initialize(ElementType elementInput) {
+    public void Initialize(ElementType elementInput, JobType jobInput) {
         elementType = elementInput;
+        jobType = jobInput;
+        
+        switch (jobInput) {
+            case JobType.Building:
+                hp = 40;
+                break;
+            case JobType.Warrior:
+                hp = 20;
+                break;
+            case JobType.Achor:
+                hp = 10;
+                break;
+            default:
+                Debug.LogWarning($"Unknown JobType: {jobInput}");
+                break;
+        }
     }
 
     private void Awake() {
         mainCamera = Camera.main;
         unitMove = GetComponent<UnitMove>();
         unitDetect = GetComponent<UnitDetect>();
+        unitBattle = GetComponent<UnitBattle>();
     }
 
     private void Start() {
@@ -33,16 +67,20 @@ public class Unit : MonoBehaviour
     }
 
     private void Update() {
-        if (closestEnemy == null)
+        if (closestEnemy == null && isAdjoinWithEnemy == false)
             unitMove.MoveUnitToForward(elementType);
-        else if (closestEnemy != null)
+        else if (closestEnemy != null && isAdjoinWithEnemy == false)
             unitMove.MoveUnitToEnemy(closestEnemy.transform);
     }
 
     private IEnumerator PerformNextBehavior(float delay)  {
         while (true)  {
             CheckIsOnScreen();
-            closestEnemy = unitDetect.FindClosestEnemy();
+            if (isAdjoinWithEnemy == false) {
+                closestEnemy = unitDetect.FindClosestEnemy();
+            }
+            else if (isAdjoinWithEnemy == true)
+                Debug.Log("Battle!");
 
             yield return new WaitForSeconds(delay);
         }
--- KingdomDefense/Assets/Scripts/Unit/UnitDetect.cs
+++ KingdomDefense/Assets/Scripts/Unit/UnitDetect.cs
@@ -6,10 +6,10 @@ using System.Linq;
 public class UnitDetect : MonoBehaviour
 {
     [SerializeField] float DetectionRadius = 2f;
+
     Unit unit;
 
-    private void Awake()
-    {
+    private void Awake() {
         unit = GetComponent<Unit>();
     }
 
@@ -18,8 +18,6 @@ public class UnitDetect : MonoBehaviour
         Collider2D[] allNearbyUnits = Physics2D.OverlapCircleAll(transform.position, DetectionRadius, 1 << gameObject.layer);
         // null 값이 아닌 collider2D만 선택해서 배열 생성
         Collider2D[] nearbyUnits = allNearbyUnits.Where(collider => collider != null).ToArray();
-
-        Unit.ElementType selfElementType = unit.elementType;
         
         Collider2D closestEnemy = null;
         float closestDistance = Mathf.Infinity;
@@ -27,6 +25,7 @@ public class UnitDetect : MonoBehaviour
         foreach (Collider2D nearbyUnit in nearbyUnits)
         {
             float distance = Vector2.Distance(transform.position, nearbyUnit.transform.position);
+            Unit.ElementType selfElementType = unit.elementType;
             Unit.ElementType nearbyUnitElementType = nearbyUnit.gameObject.GetComponent<Unit>().elementType;
 
             if (distance < closestDistance && isEnemy(selfElementType, nearbyUnitElementType))
@@ -36,16 +35,24 @@ public class UnitDetect : MonoBehaviour
             }
         }
 
-        if (closestEnemy != null)
+        if (closestEnemy != null) {
+            if (closestDistance < 0.8f)
+                unit.isAdjoinWithEnemy = true;
             return closestEnemy.gameObject;
-
+        }
         return null;
     }
 
-    private bool isEnemy(Unit.ElementType firstType, Unit.ElementType secondType)
-    {
-        bool isFirstKD = firstType <= Unit.ElementType.KD_Ice;
-        bool isSecondKD = secondType <= Unit.ElementType.KD_Ice;
-        return isFirstKD ^ isSecondKD;
+    public static bool isEnemy(Unit.ElementType firstType, Unit.ElementType secondType) {
+        if (firstType > secondType) {
+            Unit.ElementType temp = firstType;
+            firstType = secondType;
+            secondType = temp;
+        }
+
+        bool isDiffCame = (((int)firstType & 0b111000) == 0) && (((int)secondType & 0b111) == 0);
+        bool isDiffElement = ((int)firstType == 0b1) || ((int)firstType != ((int)secondType >> 3));
+
+        return isDiffCame && isDiffElement;
     }
 }

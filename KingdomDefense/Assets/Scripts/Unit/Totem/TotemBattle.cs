17e60112a7a8ffdb270c225040da9733fd473f3c
Jongeun <bubble2948@naver.com>
2023-03-15 16:17:37 +0900

    Fix Error

---
+++ KingdomDefense/Assets/Scripts/Unit/Totem/TotemBattle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotemBattle : MonoBehaviour
+{
+    public void DieUnit()
+    {
+        Destroy(gameObject);
+    }
+}
